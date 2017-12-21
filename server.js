f8c9b1459245f66c401946c08c96178c90709cb9
aneagoie <aneagoie1@gmail.com>
2017-12-21 15:40:28 -0500

    port update

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
   console.log('app is running on port 3000');
 })
