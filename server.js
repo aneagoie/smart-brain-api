67c68940a9b6e74e584bb13626150f2375c574da
aneagoie <aneagoie1@gmail.com>
2017-12-21 16:01:46 -0500

    revert

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(`it's working!`) })
+app.get('/', (req, res)=> { res.send(database.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(3000, ()=> {
   console.log('app is running on port 3000');
 })
