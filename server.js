8d9bf403080cbc23977b3ec685866c0e08757ded
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2019-01-23 12:18:14 -0800

    changes to docker setup

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : process.env.POSTGRES_HOST,
-    user : process.env.POSTGRES_USER,
-    password : process.env.POSTGRES_PASSWORD,
-    database : process.env.POSTGRES_DB
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
