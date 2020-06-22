c43de02db7f97a476e25785a481231c2c662e0e0
Andrei Neagoie <aneagoie@Andreis-MacBook-Pro-2.local>
2020-06-22 08:39:11 -0400

    clarifai api changes

--- controllers/image.js
+++ controllers/image.js
@@ -7,7 +7,9 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
   app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    // This part has been updated with the recent Clarifai changed. Used to be:
+    // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
     .then(data => {
       res.json(data);
     })
