5ec4b9f442e3c2c473be8e5abecfa2dab4324c6b
Andrei Neagoie <aneagoie@MacBook-Pro.local>
2020-10-04 09:17:57 -0400

    updates

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # SmartBrain-api - v2
-Final project for Udemy course
+Final project for ZTM course
 
 1. Clone this repo
 2. Run `npm install`
 3. Run `npm start`
-4. You must add your own API key in the `controllers/image.js` file to connect to Clarifai API.
+4. You must add your own API key in the `controllers/image.js` file to connect to Clarifai API
+5. Add your own database credentials to `server.js` line 12
 
 You can grab Clarifai API key [here](https://www.clarifai.com/)
 
--- controllers/image.js
+++ controllers/image.js
@@ -1,12 +1,21 @@
 const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai.
+//You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'YOUR API KEY HERE'
 });
 
 const handleApiCall = (req, res) => {
   app.models
+    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+    // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+    // so you would change from:
+    // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    // to:
+    // .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
