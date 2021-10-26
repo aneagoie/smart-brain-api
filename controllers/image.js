e37b7e98d17376d616f672bf8ec284c36d282ba1
Andrei Neagoie <aneagoie@MacBook-Pro-2.lan>
2021-10-26 10:55:23 -0400

    updates

--- controllers/image.js
+++ controllers/image.js
@@ -11,11 +11,11 @@ const handleApiCall = (req, res) => {
     // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
     // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
     // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-    // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+    // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
     // so you would change from:
-    // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     // to:
-    // .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+    // .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
