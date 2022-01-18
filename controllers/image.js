e555a24f83f6e9568fc6bf736fde5b247fb34bae
Andrei Neagoie <aneagoie@Andreis-MacBook-Pro-2.local>
2022-01-18 09:21:19 -0500

    update knexjs

--- controllers/image.js
+++ controllers/image.js
@@ -32,7 +32,7 @@ const handleImage = (req, res, db) => {
     // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
     // entries[0] --> this used to return the entries
     // TO
-    // entries[0].entries --> this now returns the email
+    // entries[0].entries --> this now returns the entries
     res.json(entries[0].entries);
   })
   .catch(err => res.status(400).json('unable to get entries'))
