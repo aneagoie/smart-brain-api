bdab219958e3cfcce73539b915f4ce9bea0f038f
eyalovadya <eyal.ovadya7@gmail.com>
2022-05-02 16:53:33 +0300

    Update profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,36 @@
 const handleProfileGet = (req, res, db) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
       if (user.length) {
-        res.json(user[0])
+        res.json(user[0]);
       } else {
-        res.status(400).json('Not found')
+        res.status(400).json("Not found");
       }
     })
-    .catch(err => res.status(400).json('error getting user'))
-}
+    .catch((err) => res.status(400).json("error getting user"));
+};
+
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params;
+  const { name, age, pet } = req.body.formInput;
+
+  db("users")
+    .where({ id })
+    .update({ name })
+    .then((resp) => {
+      if (resp) {
+        res.status(200).json("Success");
+      } else {
+        res.status(400).json("Unable to update");
+      }
+    })
+    .catch((err) => res.status(400).json("error updating user"));
+};
 
 module.exports = {
-  handleProfileGet
-}
+  handleProfileGet,
+  handleProfileUpdate,
+};
