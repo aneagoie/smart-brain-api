576717e75b791e3e7fdf9ca95fb56207604c2640
Laura <lmcsay@gmail.com>
2022-11-01 11:31:35 -0500

    Add dotenv

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -13,25 +14,35 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    host: '127.0.0.1',
+    user: '',
+    password: '',
+    database: 'smart-brain',
+  },
 });
 
 const app = express();
 
-app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res) => {
+  res.send(db.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('app is running on port 3000');
-})
+});
