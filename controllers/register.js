4df1ee70591a84cee5cf00ac454ecdfc89b2678b
Laura <lmcsay@gmail.com>
2022-11-01 11:32:05 -0500

    Remove comments

--- controllers/register.js
+++ controllers/register.js
@@ -1,40 +1,35 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+  console.log(req.body);
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  db.transaction((trx) => {
+    trx
+      .insert({
         hash: hash,
-        email: email
+        email: email,
       })
       .into('login')
       .returning('email')
-      .then(loginEmail => {
+      .then((loginEmail) => {
         return trx('users')
           .returning('*')
           .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
             email: loginEmail[0].email,
             name: name,
-            joined: new Date()
+            joined: new Date(),
           })
-          .then(user => {
+          .then((user) => {
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-}
-
-module.exports = {
-  handleRegister: handleRegister
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('unable to register'));
 };
 
-
+module.exports = {
+  handleRegister: handleRegister,
+};
