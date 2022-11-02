23600a15274ac1312a66f49bc3045c51c1cb6789
Laura <lmcsay@gmail.com>
2022-11-02 18:14:11 -0500

    Refactor handleSignin function to return Promises to signinAuthorication instead of responses bace to client

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,41 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
-  db.select('email', 'hash').from('login')
+  return db
+    .select('email', 'hash')
+    .from('login')
     .where('email', '=', email)
-    .then(data => {
+    .then((data) => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        return db.select('*').from('users')
+        return db
+          .select('*')
+          .from('users')
           .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
+          .then((user) => user[0])
+          .catch((err) => Promise.reject('unable to get user'));
       } else {
-        res.status(400).json('wrong credentials')
+        Promise.reject('unable to get user');
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
-}
+    .catch((err) => Promise.reject('unable to get user'));
+};
+
+const getAuthTokenId = () => {
+  console.log('auth ok');
+};
+
+const signinAuthentication = (db, brcypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization
+    ? getAuthTokenId()
+    : handleSignin(db, brcypt, req, res)
+        .then((data) => res.json(data))
+        .catch((err) => res.status(400).json(err));
+};
 
 module.exports = {
-  handleSignin: handleSignin
-}
+  signinAuthentication,
+};
