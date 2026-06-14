abad9867d29b53e1a2b2bfd132f2967231df8401
Andrei Neagoie <aneagoie@Andreis-MacBook-Pro-4.local>
2026-06-14 15:09:36 -0400

    update clarifai api to use huggingface

--- controllers/image.js
+++ controllers/image.js
@@ -77,18 +77,18 @@ const handleApiCall = (req, res) => {
 }
 
 const handleImage = (req, res, db) => {
-  // const { id } = req.body;
-  // db('users').where('id', '=', id)
-  // .increment('entries', 1)
-  // .returning('entries')
-  // .then(entries => {
-  //   // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-  //   // entries[0] --> this used to return the entries
-  //   // TO
-  //   // entries[0].entries --> this now returns the entries
-  //   res.json(entries[0].entries);
-  // })
-  // .catch(err => res.status(400).json('unable to get entries'))
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+    // entries[0] --> this used to return the entries
+    // TO
+    // entries[0].entries --> this now returns the entries
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
   return
 }
 
